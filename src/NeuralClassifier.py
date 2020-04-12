0b91f72b65f2965cd8fbccd7783f15bac40681f4
luiz <luizjosebp@gmail.com>
2020-04-12 09:19:16 -0700

    [feat] modify last layer to size 2

--- src/NeuralClassifier.py
+++ src/NeuralClassifier.py
@@ -1,39 +1,38 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
-
-def create_model(input_dim):
-    model = Sequential()
-    model.add(Dense(12, input_dim=input_dim, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # Compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+from sklearn.preprocessing import OneHotEncoder
 
 
 class NeuralClassifier:
-    def __init__(self, epochs=10, batch_size=10, verbose=1):
+    def __init__(self, epochs=10, batch_size=10, verbose=2, optimizer='adam'):
         super(NeuralClassifier, self).__init__()
         self.input_dim = None
         self.model = None
         self.epochs = epochs
         self.batch_size = batch_size
         self.verbose = verbose
+        self.optimizer = optimizer
 
-    def fit(self, X, y):
+    def fit(self, X, y, class_weights=None):
         # n_samples, n_features = X.shape
+        onehot_y = OneHotEncoder().fit_transform(y.reshape(-1, 1))
         self.input_dim = X.shape[1]
         self.model = Sequential()
         self.model.add(Dense(16, input_dim=self.input_dim, activation='relu'))
         self.model.add(Dense(8, activation='relu'))
         self.model.add(Dense(4, activation='relu'))
-        self.model.add(Dense(1, activation='sigmoid'))
+        self.model.add(Dense(2, activation='softmax'))
         # Compile model
-        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.model.compile(loss='categorical_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
 
         # Fit the model
-        self.model.fit(X, y, epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbose)
+        self.model.fit(X, onehot_y, epochs=self.epochs, batch_size=self.batch_size, verbose=self.verbose,
+                       class_weight=class_weights)
 
     def predict(self, X):
-        return np.array([1 if y > 0.5 else 0 for y in self.model.predict(X)])
+        return np.array([1 if y[1] > 0.5 else 0 for y in self.model.predict(X)])
+
+
+if __name__ == '__main__':
+    exit(0)
